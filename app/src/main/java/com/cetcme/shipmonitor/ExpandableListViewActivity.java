1af3f0861184bb9f4477c4c65b596c8722baedb9
qiuh1016 <qiuhong@qiuhs-iMac.local>
2016-12-30 11:21:06 +0800

    12301121
    1

--- app/src/main/java/com/cetcme/shipmonitor/ExpandableListViewActivity.java
+++ app/src/main/java/com/cetcme/shipmonitor/ExpandableListViewActivity.java
@@ -3,12 +3,13 @@ package com.cetcme.shipmonitor;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ImageView;
@@ -27,6 +28,8 @@ import android.widget.Toast;
  * */
 
 public class ExpandableListViewActivity extends Activity {
+
+    private String TAG = "ExpandableListViewActivity";
 
     private ExpandableListView list;
 
@@ -53,6 +56,14 @@ public class ExpandableListViewActivity extends Activity {
                 return true;
             }
         });
+
+        list.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                Log.i(TAG, "onGroupClick: " + groupPosition);
+                return false;
+            }
+        });
     }
 
     class ExpandableListAdapter extends BaseExpandableListAdapter {
@@ -64,14 +75,14 @@ public class ExpandableListViewActivity extends Activity {
         private String[] category = new String[] { "僵尸", "魔法植物", "远程植物" };
 
         //子视图显示文字
-        private String[][] subcategory = new String[][] {
+        private String[][] subCategory = new String[][] {
                 { "旗帜僵尸", "铠甲僵尸", "书生见识" },
                 { "黄金蘑菇", "贪睡蘑菇", "大头蘑菇" },
                 { "满天星",  "风车植物", "带刺植物" }
         };
 
         //子视图图片
-        public int[][] sublogos = new int[][] {
+        public int[][] subLogos = new int[][] {
                 { R.drawable.tab1, R.drawable.tab1, R.drawable.tab1},
                 { R.drawable.tab1, R.drawable.tab1, R.drawable.tab1},
                 { R.drawable.tab1, R.drawable.tab1, R.drawable.tab1}
@@ -108,24 +119,45 @@ public class ExpandableListViewActivity extends Activity {
         @Override
         public View getGroupView(int groupPosition, boolean isExpanded,
                                  View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
+//            // TODO Auto-generated method stub
+//            //定义一个LinearLayout用于存放ImageView、TextView
+//            LinearLayout view = new LinearLayout(ExpandableListViewActivity.this);
+//            //设置子控件的显示方式为水平
+//            view.setOrientation(LinearLayout.HORIZONTAL);
+//            view.setPadding(20, 20, 20, 20);
+//            //定义一个ImageView用于显示列表图片
+//            ImageView logo = new ImageView(ExpandableListViewActivity.this);
+//            logo.setPadding(50, 0, 0, 0);
+//            //设置logo的大小(50（padding）+46=96)
+//            AbsListView.LayoutParams lparParams = new AbsListView.LayoutParams(96, 50);
+//            logo.setLayoutParams(lparParams);
+//            logo.setImageResource(logos[groupPosition]);
+//            view.addView(logo);
+//            TextView textView = getTextView();
+//            textView.setTextSize(15);
+//            textView.setText(category[groupPosition]);
+//            view.addView(textView);
+
             //定义一个LinearLayout用于存放ImageView、TextView
-            LinearLayout ll = new LinearLayout(ExpandableListViewActivity.this);
+            LinearLayout view = new LinearLayout(ExpandableListViewActivity.this);
+//            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//            view.setLayoutParams(lp);
+
             //设置子控件的显示方式为水平
-            ll.setOrientation(LinearLayout.HORIZONTAL);
-            //定义一个ImageView用于显示列表图片
-            ImageView logo = new ImageView(ExpandableListViewActivity.this);
-            logo.setPadding(50, 0, 0, 0);
-            //设置logo的大小(50（padding）+46=96)
-            AbsListView.LayoutParams lparParams = new AbsListView.LayoutParams(96, 150);
-            logo.setLayoutParams(lparParams);
-            logo.setImageResource(logos[groupPosition]);
-            ll.addView(logo);
-            TextView textView = getTextView();
-            textView.setTextSize(20);
-            textView.setText(category[groupPosition]);
-            ll.addView(textView);
-            return ll;
+            view.setOrientation(LinearLayout.VERTICAL);
+            view.setPadding(60, 0, 0, 0);
+
+            View innerView = LayoutInflater.from(ExpandableListViewActivity.this).inflate(R.layout.info_list_cell, null);
+            TextView text = (TextView) innerView.findViewById(R.id.textView_in_List_Cell);
+            text.setText(category[groupPosition]);
+
+            ImageView imageView = (ImageView) innerView.findViewById(R.id.imageView_in_List_Cell);
+            imageView.setVisibility(View.INVISIBLE);
+
+            view.addView(innerView);
+
+
+            return view;
         }
 
         //取得指定分组的ID.该组ID必须在组中是唯一的.必须不同于其他所有ID（分组及子项目的ID）.
@@ -153,7 +185,7 @@ public class ExpandableListViewActivity extends Activity {
         @Override
         public int getChildrenCount(int groupPosition) {
             // TODO Auto-generated method stub
-            return subcategory[groupPosition].length;
+            return subCategory[groupPosition].length;
         }
 
         //取得显示给定分组给定子位置的数据用的视图
@@ -161,22 +193,41 @@ public class ExpandableListViewActivity extends Activity {
         public View getChildView(int groupPosition, int childPosition,
                                  boolean isLastChild, View convertView, ViewGroup parent) {
             // TODO Auto-generated method stub
+//            //定义一个LinearLayout用于存放ImageView、TextView
+//            LinearLayout ll = new LinearLayout(ExpandableListViewActivity.this);
+//            //设置子控件的显示方式为水平
+//            ll.setOrientation(LinearLayout.HORIZONTAL);
+//            //定义一个ImageView用于显示列表图片
+//            ImageView logo = new ImageView(ExpandableListViewActivity.this);
+//            logo.setPadding(0, 0, 0, 0);
+//            //设置logo的大小
+//            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(40, 40);
+//            logo.setLayoutParams(lp);
+//            logo.setImageResource(subLogos[groupPosition][childPosition]);
+//            ll.addView(logo);
+//            TextView textView = getTextView();
+//            textView.setText(subCategory[groupPosition][childPosition]);
+//            ll.addView(textView);
+
             //定义一个LinearLayout用于存放ImageView、TextView
-            LinearLayout ll = new LinearLayout(ExpandableListViewActivity.this);
+            LinearLayout view = new LinearLayout(ExpandableListViewActivity.this);
+
             //设置子控件的显示方式为水平
-            ll.setOrientation(LinearLayout.HORIZONTAL);
-            //定义一个ImageView用于显示列表图片
-            ImageView logo = new ImageView(ExpandableListViewActivity.this);
-            logo.setPadding(0, 0, 0, 0);
-            //设置logo的大小
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(40, 40);
-            logo.setLayoutParams(lp);
-            logo.setImageResource(sublogos[groupPosition][childPosition]);
-            ll.addView(logo);
-            TextView textView = getTextView();
-            textView.setText(subcategory[groupPosition][childPosition]);
-            ll.addView(textView);
-            return ll;
+            view.setOrientation(LinearLayout.VERTICAL);
+            view.setPadding(100, 0, 0, 0);
+
+            View innerView = LayoutInflater.from(ExpandableListViewActivity.this).inflate(R.layout.info_list_cell, null);
+            TextView text = (TextView) innerView.findViewById(R.id.textView_in_List_Cell);
+            text.setText(subCategory[groupPosition][childPosition]);
+            text.setTextSize(13);
+
+            ImageView imageView = (ImageView) innerView.findViewById(R.id.imageView_in_List_Cell);
+            imageView.setVisibility(View.INVISIBLE);
+
+            view.addView(innerView);
+            view.setBackgroundColor(Color.rgb(219, 219, 219));
+
+            return view;
         }
         //取得给定分组中给定子视图的ID. 该组ID必须在组中是唯一的.必须不同于其他所有ID（分组及子项目的ID）.
         @Override
@@ -188,7 +239,7 @@ public class ExpandableListViewActivity extends Activity {
         @Override
         public Object getChild(int groupPosition, int childPosition) {
             // TODO Auto-generated method stub
-            return subcategory[groupPosition][childPosition];
+            return subCategory[groupPosition][childPosition];
         }
     }
 
--- app/src/main/java/com/cetcme/shipmonitor/Fragment/ShipsFragment.java
+++ app/src/main/java/com/cetcme/shipmonitor/Fragment/ShipsFragment.java
@@ -1,15 +1,30 @@
 package com.cetcme.shipmonitor.Fragment;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.cetcme.shipmonitor.ExpandableListViewActivity;
 import com.cetcme.shipmonitor.R;
 
 
 public class ShipsFragment extends Fragment {
+
+    private String TAG = "ShipsFragment";
+
+    private ExpandableListView list;
 
     public static ShipsFragment newInstance(String param1) {
         ShipsFragment fragment = new ShipsFragment();
@@ -35,7 +50,211 @@ public class ShipsFragment extends Fragment {
         Bundle bundle = getArguments();
         String agrs1 = bundle.getString("agrs1");
 
+        list = (ExpandableListView) view.findViewById(R.id.list);
+
+        //创建一个BaseExpandableListAdapter对象
+        final ShipsFragment.ExpandableListAdapter adapter = new ShipsFragment.ExpandableListAdapter();
+
+        list.setAdapter(adapter);
+        //为ExpandableListView的子列表单击事件设置监听器
+        list.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getActivity(), "你单击了："
+                        +adapter.getChild(groupPosition, childPosition), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        list.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                Log.i(TAG, "onGroupClick: " + groupPosition);
+                return false;
+            }
+        });
+
         return view;
+    }
+
+    class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+        //设置组视图的图片
+        int[] logos = new int[] { R.drawable.tab1, R.drawable.tab2,R.drawable.tab3};
+
+        //设置组视图的显示文字
+        private String[] category = new String[] { "长风77", "建工86", "xxx" };
+
+        //子视图显示文字
+        private String[][] subCategory = new String[][] {
+                { "左机：宁动6700", "右机：宁动6700" },
+                { "左机：宁动6700", "右机：宁动6700" },
+                { "左机：宁动6700", "右机：宁动6700" },
+        };
+
+        //子视图图片
+        public int[][] subLogos = new int[][] {
+                { R.drawable.tab1, R.drawable.tab1, R.drawable.tab1},
+                { R.drawable.tab1, R.drawable.tab1, R.drawable.tab1},
+                { R.drawable.tab1, R.drawable.tab1, R.drawable.tab1}
+        };
+
+        //定义一个显示文字信息的方法
+        TextView getTextView(){
+            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 64);
+            TextView textView = new TextView(getActivity());
+            //设置 textView控件的布局
+            textView.setLayoutParams(lp);
+            //设置该textView中的内容相对于textView的位置
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            //设置txtView的内边距
+            textView.setPadding(36, 0, 0, 0);
+            //设置文本颜色
+            textView.setTextColor(Color.BLACK);
+            return textView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            // TODO Auto-generated method stub
+            return true;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            // TODO Auto-generated method stub
+            return true;
+        }
+
+        //取得用于显示给定分组的视图. 这个方法仅返回分组的视图对象
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded,
+                                 View convertView, ViewGroup parent) {
+//            // TODO Auto-generated method stub
+//            //定义一个LinearLayout用于存放ImageView、TextView
+//            LinearLayout view = new LinearLayout(getActivity());
+//            //设置子控件的显示方式为水平
+//            view.setOrientation(LinearLayout.HORIZONTAL);
+//            view.setPadding(20, 20, 20, 20);
+//            //定义一个ImageView用于显示列表图片
+//            ImageView logo = new ImageView(getActivity());
+//            logo.setPadding(50, 0, 0, 0);
+//            //设置logo的大小(50（padding）+46=96)
+//            AbsListView.LayoutParams lparParams = new AbsListView.LayoutParams(96, 50);
+//            logo.setLayoutParams(lparParams);
+//            logo.setImageResource(logos[groupPosition]);
+//            view.addView(logo);
+//            TextView textView = getTextView();
+//            textView.setTextSize(15);
+//            textView.setText(category[groupPosition]);
+//            view.addView(textView);
+
+            //定义一个LinearLayout用于存放ImageView、TextView
+            LinearLayout view = new LinearLayout(getActivity());
+//            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//            view.setLayoutParams(lp);
+
+            //设置子控件的显示方式为水平
+            view.setOrientation(LinearLayout.VERTICAL);
+            view.setPadding(60, 0, 0, 0);
+
+            View innerView = LayoutInflater.from(getActivity()).inflate(R.layout.info_list_cell, null);
+            TextView text = (TextView) innerView.findViewById(R.id.textView_in_List_Cell);
+            text.setText(category[groupPosition]);
+
+            ImageView imageView = (ImageView) innerView.findViewById(R.id.imageView_in_List_Cell);
+            imageView.setVisibility(View.INVISIBLE);
+
+            view.addView(innerView);
+
+
+            return view;
+        }
+
+        //取得指定分组的ID.该组ID必须在组中是唯一的.必须不同于其他所有ID（分组及子项目的ID）.
+        @Override
+        public long getGroupId(int groupPosition) {
+            // TODO Auto-generated method stub
+            return groupPosition;
+        }
+
+        //取得分组数
+        @Override
+        public int getGroupCount() {
+            // TODO Auto-generated method stub
+            return category.length;
+        }
+
+        //取得与给定分组关联的数据
+        @Override
+        public Object getGroup(int groupPosition) {
+            // TODO Auto-generated method stub
+            return category[groupPosition];
+        }
+
+        //取得指定分组的子元素数.
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            // TODO Auto-generated method stub
+            return subCategory[groupPosition].length;
+        }
+
+        //取得显示给定分组给定子位置的数据用的视图
+        @Override
+        public View getChildView(int groupPosition, int childPosition,
+                                 boolean isLastChild, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+//            //定义一个LinearLayout用于存放ImageView、TextView
+//            LinearLayout ll = new LinearLayout(getActivity());
+//            //设置子控件的显示方式为水平
+//            ll.setOrientation(LinearLayout.HORIZONTAL);
+//            //定义一个ImageView用于显示列表图片
+//            ImageView logo = new ImageView(getActivity());
+//            logo.setPadding(0, 0, 0, 0);
+//            //设置logo的大小
+//            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(40, 40);
+//            logo.setLayoutParams(lp);
+//            logo.setImageResource(subLogos[groupPosition][childPosition]);
+//            ll.addView(logo);
+//            TextView textView = getTextView();
+//            textView.setText(subCategory[groupPosition][childPosition]);
+//            ll.addView(textView);
+
+            //定义一个LinearLayout用于存放ImageView、TextView
+            LinearLayout view = new LinearLayout(getActivity());
+
+            //设置子控件的显示方式为水平
+            view.setOrientation(LinearLayout.VERTICAL);
+            view.setPadding(100, 0, 0, 0);
+
+            View innerView = LayoutInflater.from(getActivity()).inflate(R.layout.info_list_cell, null);
+            TextView text = (TextView) innerView.findViewById(R.id.textView_in_List_Cell);
+            text.setText(subCategory[groupPosition][childPosition]);
+            text.setTextSize(13);
+
+            ImageView imageView = (ImageView) innerView.findViewById(R.id.imageView_in_List_Cell);
+            imageView.setVisibility(View.INVISIBLE);
+
+            view.addView(innerView);
+            int backColor = 230;
+            view.setBackgroundColor(Color.rgb(backColor, backColor, backColor));
+
+            return view;
+        }
+        //取得给定分组中给定子视图的ID. 该组ID必须在组中是唯一的.必须不同于其他所有ID（分组及子项目的ID）.
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            // TODO Auto-generated method stub
+            return childPosition;
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            // TODO Auto-generated method stub
+            return subCategory[groupPosition][childPosition];
+        }
     }
 
 }
